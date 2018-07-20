a01f6ab2d133a92b7916272d9cb729dbae5a7a9f
Matthew Feng <mattfeng@mit.edu>
2018-07-20 04:19:49 -0400

    Add templating, materialize

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,13 +36,18 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (req.app.get('env') === 'development') {
+    res.locals.message = err.message;
+    res.locals.error = err;
+
+    res.render('error');
+  } else {
+    res.render('public-error', {
+      title: 'Uh oh!',
+      mini: true
+    });
+  }
 });
 
 module.exports = app;
