9ff8cac2fbff349b8e1db225251e7d0f33bc44ba
Matthew Feng <mattfeng@mit.edu>
2018-08-07 21:28:29 +0300

    Add slides

--- routes/slides.js
+++ routes/slides.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const slideResources = require(__dirname + '/jsons/slides.json');
 
 router.get('/', function(req, res, next) {
   res.render('slides', {
     title: 'Slides',
     mini: true,
-    activeLink: 'slides'
+    activeLink: 'slides',
+    slideResources: slideResources['slides']
   })
 });
 
